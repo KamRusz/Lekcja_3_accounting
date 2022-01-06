ac8678bd7b9690b0e71f4dc5843195b68bb63b4a
Kamil <kamilrusztyn@gmail.com>
2022-01-06 14:55:09 +0100

    dopracowuje sys.argv

--- accountant.py
+++ accountant.py
@@ -6,64 +6,65 @@ suma_saldo = 0   #do operacji "konto"
 while True:
     argument = str(input())
     if argument not in dozwolone_operacje:
-       print("błąd - niedozwlona akcja\nprogram zakończy działanie")
+       print("błąd - niedozwolona akcja\nprogram zakończy działanie")
        break
     if argument == "saldo":                                       #funkcja saldo
-        wartosc = int(input("wartość"))
+        wartosc = int(input())
         if suma_saldo:
             suma_saldo += wartosc
         else:
             suma_saldo = wartosc   
-        komentarz = str(input("komentarz"))
+        komentarz = str(input())
         operacje.append([argument, wartosc, komentarz])
-        #operacje.append(suma_saldo)
-        #operacje.append(komentarz)
     elif argument =="zakup":                                     #funkcja zakup
-        identyfikator = str(input("id"))
-        cena = int(input("cena"))
-        liczba = int(input("ilosc"))
+        identyfikator = str(input())
+        cena = int(input())
+        liczba = int(input())
         if suma_saldo - cena * liczba <0:
             print("\nbłąd - saldo nie może być ujemne\n")
             continue
         else:
             suma_saldo -= cena * liczba 
-            if towary[identyfikator]:
+            if identyfikator in towary:
                 towary[identyfikator] += liczba
             else:
-                towary[identyfikator] = liczba      
-            operacje.append(argument)
-            operacje.append(identyfikator)
-            operacje.append(cena)
-            operacje.append(liczba)
+                towary[identyfikator] = liczba     
+            operacje.append([argument, identyfikator, cena, liczba])
     elif argument =="sprzedaz":                                 #funkcja sprzedaż
-        identyfikator = str(input("id"))
+        identyfikator = str(input())
         if identyfikator not in towary:
             print (f"\nnie mamy na stanie {identyfikator}\n") 
             continue   
         else:
-            cena = int(input("cena"))
-            liczba = int(input("ilosc"))
+            cena = int(input())
+            liczba = int(input())
             if towary[identyfikator] - liczba < 0:
                 print (f"nie wystarczająca ilość {identyfikator} na stanie")
                 continue
             else:        
                 towary[identyfikator] -= liczba
                 suma_saldo += cena * liczba
-                operacje.append(liczba)
-                operacje.append(argument)
-                operacje.append(identyfikator)
-                operacje.append(komentarz)
+                operacje.append([argument, identyfikator, cena, liczba])
     elif argument =="stop":
         operacje.append(argument)
         break   
-    
-if sys.argv[1] =="konto":                                       #funkcja konto
+
+if sys.argv[1] =="saldo":  
+    print (suma_saldo*2)
+elif sys.argv[1] =="sprzedaz":   
+    print (suma_saldo*3)  
+elif sys.argv[1] =="zakup": 
+    print (suma_saldo)
+elif sys.argv[1] =="konto":                 #działa
     print (f"\nwartość saldo:{suma_saldo}") 
-elif sys.argv[1] =="magazyn":
-    print (f"\nstan magazynu: \n {towary}")                     #funkcja magazyn    
-elif sys.argv[1] =="przeglad":                                  #funkcja przeglad
+elif sys.argv[1] =="magazyn":       
+    print (f"\nstan magazynu: \n {towary}")   
+elif sys.argv[1] =="przeglad":              #działa
     for i in range (int(sys.argv[2]),int(sys.argv[3])+1):
         print(*operacje[i], sep = "\n")
+#print (operacje[-1])
+
+        
 
 
 
